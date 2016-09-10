103d27146f63d85122e0936957054157b7d55a9d
hughest64 <hughest64@gmail.com>
2016-09-10 11:29:55 -0500

    Update systests.py
    Added set menu functionality for setting the timer

--- systests.py
+++ systests.py
@@ -1,17 +1,67 @@
-import wx # wxpython is being used for our gui
+import wx # using wxpython for the GUI
 from timer import * # the brewing Timer class
 """
 TODO: !!!
 - add box for timer display
-- menu option to set timer time (dialog box?)
 - add hop add parsing to timer(boil) !!!
+- layout management and style
 """
 ### CONSTANTS ###
 SIZE = ((640, 480))
 TITLE = 'Brewsys'
 TIMER = Timer()
 
-class World(wx.Frame):  #Creating our Window
+class SetTimer(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        super(SetTimer, self).__init__(*args, **kwargs)
+
+        #buttons
+        svbtn = wx.Button(self, label='Save', pos=(20, 120))
+        clbtn = wx.Button(self, label='Close', pos=(100, 120))
+
+        #static text labels
+        st1 = wx.StaticText(self, label='Min', pos=(50, 20))
+        st2 = wx.StaticText(self, label='Sec', pos=(50, 70))
+
+        #spin boxes for setting the timer mn/sec vals
+        self.sc1 = wx.SpinCtrl(self, value='0', pos=(80, 15),
+                               size=(60, -1), min=0, max=120)
+        self.sc2 = wx.SpinCtrl(self, value='0', pos=(80, 65),
+                               size=(60, -1), min=0, max=59)
+
+        #button bindings
+        svbtn.Bind(wx.EVT_BUTTON, self.OnSave)
+        self.Bind(wx.EVT_BUTTON, self.OnSaveClick)
+        clbtn.Bind(wx.EVT_BUTTON, self.OnCancel)
+
+        self.SetSize((250, 250))
+        self.SetTitle("Set Timer")
+        self.Centre()
+
+    def ShowTime(self, e):
+        """ Show the widow """
+        self.Show(True)
+
+    def OnSave(self, e):
+        """
+        Apply the new timer values and close the window
+        and propigate the event to the class.
+        """
+        mn = self.sc1.GetValue()
+        sec = self.sc2.GetValue()
+        TIMER.Set(mn, sec)
+        e.Skip()
+        self.Close()
+
+    def OnSaveClick(self, e):
+        """ Propigate the event to the World class """
+        e.Skip()
+
+    def OnCancel(self, e):
+        """ Close the window withough making changes """
+        self.Close(True)
+
+class World(wx.Frame):
 
     def __init__(self, *args, **kwargs):
         super(World, self).__init__(*args, **kwargs)
@@ -20,14 +70,14 @@ class World(wx.Frame):  #Creating our Window
 
     def InitUI(self):
         """ Set up the interface """
-        #panel = SetTimer(self)
+        panel = SetTimer(self) # the should be creted from the SetTimer class
 
         # buttons
-        self.stbtn = wx.Button(self, label='Start', pos=(200,150))
-        rbtn = wx.Button(self, label='Reset', pos=(275,150))
-        cbtn = wx.Button(self, label='Close', pos=(350,150))
+        self.stbtn = wx.Button(self, label='Start', pos=(200,125))
+        rbtn = wx.Button(self, label='Reset', pos=(275,125))
+        cbtn = wx.Button(self, label='Close', pos=(350,125))
 
-        # creating the menu system
+        # the menu system
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
         fitemOne = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
@@ -40,20 +90,20 @@ class World(wx.Frame):  #Creating our Window
         font = wx.Font(30, wx.ROMAN, wx.NORMAL, wx.BOLD)
         self.dc.SetFont(font)
 
-        #intializing the timer
-        TIMER.Set(0, 10) # this needs it's own method and binding !!!
+        #intializing the wx.Timer()
         self.wxtimer = wx.Timer(self)
-        #settimer = SetTimer(self)
+        settimer = SetTimer(self)
 
         # event bindings
         self.Bind(wx.EVT_MENU, self.OnClose, fitemOne)
-        #self.Bind(wx.EVT_MENU, settimer.ShowTime, fitemTwo)
+        self.Bind(wx.EVT_MENU, settimer.ShowTime, fitemTwo)
         self.Bind(wx.EVT_TIMER, self.OnTimer, self.wxtimer)
         self.stbtn.Bind(wx.EVT_BUTTON, self.OnRunning)
         cbtn.Bind(wx.EVT_BUTTON, self.OnClose)
         rbtn.Bind(wx.EVT_BUTTON, self.OnReset)
+        self.Bind(wx.EVT_BUTTON, self.OnSaveClick)
 
-        # setting display details
+        # set display details
         self.SetSize(SIZE)
         self.SetTitle(TITLE)
         self.Centre()
@@ -64,49 +114,47 @@ class World(wx.Frame):  #Creating our Window
         self.dc.DrawText(self.vals['display'], 260, 50)
 
     def OnRefresh(self, e):
+        """ Used to reset the display. """
         self.vals = TIMER.GetDisplay()
         self.dc.Clear()
         self.dc.DrawText(self.vals['display'], 260, 50)
 
     def OnTimer(self, e):
-        """ method to draw the timer """
-        if self.vals['display'] == '00:00':
-            TIMER.Stop()
-            self.wxtimer.Stop()
-            self.stbtn.SetLabel('Start')
-
-        elif self.vals['mn'] >= 0 and TIMER.GetStatus():
+        """ Method to draw the timer """
+        if self.vals['mn'] >= 0 and TIMER.GetStatus():
             # decrement the timer
             TIMER.Run()
+            # reset the display
             self.OnRefresh(e)
+            # stop once we are at '00:00'
+            if self.vals['display'] == '00:00':
+                TIMER.Stop()
+                self.wxtimer.Stop()
+                self.stbtn.SetLabel('Start')
 
     def OnRunning(self, e):
         """ Start and stop the timer. """
-        #if self.vals['Display'] == '00:00':
-
+        # start the timer unless we are a '00:00'
         if not TIMER.GetStatus() and self.vals['display'] != '00:00':
             self.stbtn.SetLabel('Pause')
             self.wxtimer.Start(1000)
             TIMER.Start()
-
+        # stop the timer
         else:
             TIMER.Stop()
             self.wxtimer.Stop()
             self.stbtn.SetLabel('Start')
 
-    def OnSet(self, mn, sec, e):
-        #print mn, sec
-        TIMER.Set(mn, sec)
-        self.OnRefresh(e)
-        #self.vals = TIMER.GetDisplay()
-        #self.dc.Clear()
-        #self.dc.DrawText(self.vals['display'], 260, 50)
-
     def OnReset(self, e):
-        """ reset the timer """
+        """ Reset the timer """
         self.stbtn.SetLabel('Start')
         TIMER.Reset()
         self.OnRefresh(e)
+
+    def OnSaveClick(self, e):
+        """ Received wx.EVT_BUTTON event from SetTimer class. """
+        self.OnRefresh(e)
+        e.Skip()
 
     def OnClose(self, e):
         """ close the app """
